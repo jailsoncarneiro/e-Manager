d8412fe694d853e66393b19d8976bb1b7739d065
Jailson Carneiro <jailsoncarneiro@hotmail.com>
2021-01-13 21:31:31 -0300

    mais mudança key no welcome 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Patrocinador
+                                Patrocinador Bacana
                             </a>
                         </div>
                     </div>
