73cec920aec649a7b38743330190933f0bc55a98
Jailson Carneiro <jailsoncarneiro@hotmail.com>
2021-01-20 00:30:52 -0300

    Adicionado o dominio prodsite.pacetosoftware.com.br na configuração do sanctm

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,local.tgcsistemas.com/laravel/emanager/public,pacetosoftware.com.br'
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,local.tgcsistemas.com/laravel/emanager/public,prodsite.pacetosoftware.com.br'
     )),
 
     /*
